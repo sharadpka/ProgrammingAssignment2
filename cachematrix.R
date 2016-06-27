bf291c6deb27824cb8a067c046b8889e04f27a39
Sharad <sharadpka@gmail.com>
2016-06-27 16:46:36 -0400

    Modified the file for Programming Assignment 2
    Added makeCachematrix and cacheSolve functions to make and compute the
    inverse of a matrix if not available already in cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCacheMatrix saves a matrix in memory
+## cacheSolve calculates the inverse of a matrix or gets it from cache, if available
 
 ## Write a short comment describing this function
+## Makes and saves a matrix in memory
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        inverse <- NULL
+            set <- function(y)  {
+              x <<- y
+              inverse <<- NULL
+            }
+            get <- function () x
+            setinverse <- function(Inverse) inverse <<- Inverse
+            getinverse <- function() inverse
+            list (set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
+## Calcualtes the inverse of a matrix if not available in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached matrix")
+        return(inverse)
+    }
+    message("inverse not in memory and will be calculated") 
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
